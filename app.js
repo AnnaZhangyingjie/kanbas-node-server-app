c6b3e8df2173ed364ac15c3c4ac26ad595fb7a54
Yingjie Zhang <zhang.yingji@northeastern.edu>
2023-12-04 19:43:25 -0800

    connect node to mongoDB

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ import UserRoutes from "./users/routes.js";
 import "dotenv/config";
 import session from "express-session";
 
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
 
 //creates an instance of an Express application
 const app = express();
