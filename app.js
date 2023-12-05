b994ea5a8d81becf630ca3204a0f1909e6327569
Yingjie Zhang <zhang.yingji@northeastern.edu>
2023-12-04 18:37:53 -0800

    update a6

--- app.js
+++ app.js
@@ -6,16 +6,47 @@ import ModuleRoutes from "./modules/routes.js";
 import AssignmentsRoutes from './assignments/routes.js';
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+
+import "dotenv/config";
+import session from "express-session";
+
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
 //creates an instance of an Express application
 const app = express();
 
 //allow cross-origin requests
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+}));
+
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+
+app.use(
+    session(sessionOptions)
+);
+
 
 // parses incoming requests with JSON payloads
 //JSON data sent by the client would be readily accessible in req.body
 app.use(express.json());
+
+UserRoutes(app);
 
 //define various routes for your application
 ModuleRoutes(app);
